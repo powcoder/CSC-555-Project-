f11536f93ed36234a3c6fe4309a5c3e23f5f46e0
powcoder <powcoder@163.com>
2022-11-22 13:48:23 +0800

    CSC 555 Project

---
+++ lineCustReduceGroup.py
@@ -0,0 +1,34 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#!/usr/bin/python
+import sys
+
+curr_id = None
+curr_tot = 0
+id = None
+
+# The input comes from standard input (line by line)
+for line in sys.stdin:
+	# parse the line and split it by '\t'
+	line = line.strip().plit('\t')
+
+	# grab the key
+	id = line[0] + ' ' + line[1]
+		
+	# grab the value (int)
+	val = int(line[2])
+
+	if curr_id == id:
+		curr_tot += val
+	else:
+		if curr_id: # output the sum, single key completed
+			print '%s\t%s\t%d' % (line[0], line[1], curr_tot)
+
+			curr_id = id
+			curr_tot = val
+
+# output the last key
+if curr_id == id:
+    print '%s\t%s\t%d' % (line[0], line[1], curr_tot)
---
+++ vehicleMapJoin.py
@@ -0,0 +1,22 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#!/usr/bin/python
+import sys
+ 
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+         
+    line = line.strip()
+    split = line.split(',')
+         
+    if split[0].endswith(' AM') or split[0].endswith(' PM'):
+        if len(split[3]) > 0: # There are some blank licenses
+            firstThree = split[3][:3]
+            print firstThree, '\t', 'RelocatedVehicle'
+    else: 
+        if len(split[5]) > 0: # There are some blank licenses
+            firstThree = split[5][:3]
+            print firstThree, '\t', 'TowedVehicle'
+
